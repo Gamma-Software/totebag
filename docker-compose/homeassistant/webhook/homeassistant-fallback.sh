25f263ac8ae410669344f74b669f70b072dee7fc
Valenti Rudloff <valentin.rudloff.perso@gmail.com>
2022-12-04 22:18:21 +0100

    update fallback

--- docker-compose/homeassistant/webhook/homeassistant-fallback.sh
+++ docker-compose/homeassistant/webhook/homeassistant-fallback.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 if [ $(echo $1 | jq -r '.heartbeat.status') = "0" ]; then
     echo "Home Assistant is offline"
-    docker run -d --name homeassistant --privileged --restart=unless-stopped -e TZ=Europe/Paris -v /mnt/data/workspace/boilerplates/docker-compose/homeassistant/HomeAssistantConfig:/config --network=host ghcr.io/home-assistant/home-assistant:stable
-    exit 0    
+    #docker run -d --name homeassistant --privileged --restart=unless-stopped -e TZ=Europe/Paris -v /mnt/data/workspace/boilerplates/docker-compose/homeassistant/config:/config --network=host ghcr.io/home-assistant/home-assistant:stable
+    docker start homeassistant
+    exit 0
 else
     echo "Home Assistant is online"
-    docker rm homeassistant --force | true
+    #docker rm homeassistant --force | true
+    docker stop homeassistant
     exit 0
 fi
